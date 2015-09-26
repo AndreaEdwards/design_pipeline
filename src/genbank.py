a66e1c948264f2d3582f84536a7e9d9670570f3a
Andrea Halweg-Edwards <andrea.edwards@colorado.edu>
2015-09-25 18:33:25 -0600

    added function to output fasta from genbank file

--- src/genbank.py
+++ src/genbank.py
@@ -1,5 +1,5 @@
 import os
-from Bio import Entrez
+from Bio import Entrez, SeqIO
 from src.util import FileHandlers
 
 class GenBank:
@@ -22,11 +22,20 @@ class GenBank:
 		save_file.write(self.handle)
 		save_file.close()
 
+	def _output_fasta(self):
+		file_handlers = FileHandlers()
+		file_paths = file_handlers.search_directory()
+		gb_files = file_handlers.find_files(file_paths, 'gb')
+		for gb_file in gb_files:
+			if self.genbank_record_number == file_handlers.get_file_name(gb_file).split('.')[0]:
+				SeqIO.convert(gb_file, 'genbank', (file_handlers.get_file_name(gb_file) + '.fasta'), 'fasta')
+
 	def fetch_record(self, genbank_id):
 		self.genbank_record_number = str(genbank_id)
 		handle = Entrez.efetch(db="protein", id=self.genbank_record_number, rettype="gb", retmode="text")
 		self.handle = handle.read()
 		handle.close()
 		self._save_genbank_record()
+		self._output_fasta()
 
 
